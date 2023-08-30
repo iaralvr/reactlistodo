884829821552e81252f9cc1274d16a76e322e749
iaralvr <iaralanzasenai@gmail.com>
2023-08-30 09:30:26 -0300

    Status Funcionando

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 function App() {
 
   const [ listaTarefas, setListaTarefas ] = useState ([]);
-  const [tarefa, setTarefa ] = useState ( {id: '' , texto: ''});
+  const [tarefa, setTarefa ] = useState ( {id: '' , texto: "" , status: ""});
 
   function addTarefa()
   {
@@ -11,28 +11,42 @@ function App() {
       setListaTarefas([...listaTarefas, tarefa ]);
     }
   }
-  useEffect(() =>{
-      setTarefa( { id: "", texto: "" });
-  }, [listaTarefas])
+
 
   function excluirTarefa(id) {
     const novalista = listaTarefas.filter( tarefa => tarefa.id !== id);
     setListaTarefas(novalista);
   }
-   return (
+
+
+  function concluirTarefa(id,status) {
+    const index = listaTarefas.findIndex( (tarefa) => tarefa.id === id );
+   
+    listaTarefas[index].status = !status;
+    setListaTarefas([...listaTarefas]);
+    
+  }
+
+
+  useEffect(() =>{
+    setTarefa( { id: "", texto: "", status:"" });
+}, [listaTarefas])
+
+
+  return (
     <>
     <header>
       <h1>TO DO LIST</h1>
     </header>
       <div>
-        <input type="text" nome= "tarefa" placeholder="Digite sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefa( {id: Math.random(), texto: e.target.value })} />
+        <input type="text" nome= "tarefa" placeholder="Digite sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefa( {id: Math.random(), texto: e.target.value, status: false })} />
         <button onClick={addTarefa}>Add itens</button>
     
       </div>
       <div>
         <ul> 
           {listaTarefas.map( (item, index ) => (
-           <li key={index}>{item.texto} <button onClick={() => excluirTarefa(item.id) }>Excluir</button></li>
+           <li key={item.id}>{item.texto}<button  onClick={() => concluirTarefa(item.id,item.status) }>{item.status ? 'Concluida' : 'Não concluida'}</button> <button onClick={() => excluirTarefa(item.id) }>Excluir</button></li>
           ))}
         </ul>
       </div>
@@ -41,5 +55,5 @@ function App() {
   );
 }
 
-export default App;
 
+export default App;
